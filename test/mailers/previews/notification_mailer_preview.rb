7f14cd8509e62e83f32fc40a0338b28194127e69
Justin Veirs <justinveirs@gmail.com>
2014-07-08 10:40:15 -0600

    added not

--- test/mailers/previews/notification_mailer_preview.rb
+++ test/mailers/previews/notification_mailer_preview.rb
@@ -1,4 +1,10 @@
 # Preview all emails at http://localhost:3000/rails/mailers/notification_mailer
+# this is where we set up the previews.
 class NotificationMailerPreview < ActionMailer::Preview
+
+  def contact_form
+    params = {user_email: 'billy@cheeze.info', in_regard_to: 'eating cheeze', content: 'BLARG! dude'}
+    NotificationMailer.contact_form(params)
+  end
 
 end
